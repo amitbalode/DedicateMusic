798a759246127984e8d14a4171fc5ebe2a431529
Amit Balode <amit.balode@gmail.com>
2011-05-05 17:44:26 -0700

    testing youtube code, making reusable functions and configurations

--- learning/test.php
+++ learning/test.php
@@ -1,29 +1,69 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<input type="text" value="lata" id="searchbox" name="searchbox"/>
 <script type="text/javascript">
 DM = {};
 DM.App = {};
-DM.App.search = {};
+DM.App.mediaSource = [];
+DM.App.mediaSource['yt'] = {
+	'id': 'youtube', 
+	'fetchUrl': 'http://gdata.youtube.com/feeds/api/videos?q='
+};
 
-<?//Configuration ?>
+<?php // ------- Defining Data Structures --------------- ?>
+DM.App.struct = {};
+DM.App.struct.song = function(){
+	this.songId = '';
+	this.songTitle = '';
+	this.songUrl = '';
+	this.songMedia = '';
+};
+<?php // ------------------------------------------------ ?>
+
+<?php // ------- Defining Search Configuration HTML ids and functions --------------- ?>
+DM.App.search = {};
+<?php //Configuration ?>
 DM.App.search.searchButton = "searchbox";
 
-DM.App.search.homeTabCallback = function(data){
-	$.each(data.feed.entry, function(i, item) {
-		 var title = item['title']['$t'];
-		 var video = item['id']['$t'];
-		 video = video.replace('http://gdata.youtube.com/feeds/api/videos/','http://www.youtube.com/watch?v=');  //replacement of link
-		 videoID = video.replace('http://www.youtube.com/watch?v=',''); // removing link and getting the video ID
-		 console.log(videoID);
-	});
+<?php //Function called when user click on Search button/ or type in search button ?>
+DM.App.search.searchButtonClicked = function(){
+	var query = $("#"+DM.App.search.searchButton).val();
+	DM.App.search.ytFetchSongs(query);
 };
 
-DM.App.search.searchButtonClicked = function(){
-	var txt = $("#"+DM.App.search.searchButton).val();
+
+<?php //Function called to fetch songs from youtube ?>
+DM.App.search.ytFetchSongs = function(query){
+	var media = DM.App.mediaSource['yt'];
+	var songs = [];
+	$.getJSON(media.fetchUrl+query+'&alt=json-in-script&callback=?&max-results=12&start-index=1',
+		function(data){
+    		$.each(data.feed.entry, function(i, item) {
+
+    			var video = item['id']['$t'];
+			  video = video.replace('http://gdata.youtube.com/feeds/api/videos/','http://www.youtube.com/watch?v=');  //replacement of link
+			  videoID = video.replace('http://www.youtube.com/watch?v=',''); // removing link and getting the video ID
+
+			  song = new DM.App.struct.song();
+			  song.songId = videoID;
+			  song.songTitle = item['title']['$t'];
+			  song.songUrl = video;
+			  song.songMedia = media.id;
+    			songs.push(song);	
+    		});
+    		DM.App.homeTab.populate(songs);
+		});
 };
-DM.App.search.fetchSongs = function(){
-	$.getJSON('http://gdata.youtube.com/feeds/api/videos?q=lata&alt=json-in-script&callback=?&max-results=12&start-index=1',DM.App.search.homeTabCallback);
+<?php // ------------------------------------------------ ?>
+
+
+<?php // ------- Defining Home Tab functionality --------------- ?>
+DM.App.homeTab = {};
+DM.App.homeTab.populate = function(data){
+  for(var i = 0; i < data.length; i++) {
+	  console.log(data[i]);
+  }	
 };
-DM.App.search.fetchSongs();
+<?php // ------------------------------------------------ ?>
+
+DM.App.search.searchButtonClicked();
 </script>
-<?php
-  echo 1;
