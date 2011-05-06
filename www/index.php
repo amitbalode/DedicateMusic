c709b1b1fd31dbef769f0cde351b4142d274f9cf
Amit Balode <amit.balode@gmail.com>
2011-05-05 18:33:24 -0700

    adding web root folder

---
+++ www/index.php
@@ -0,0 +1,82 @@
+<?php 
+  /**
+   * Authors: Amit Balode, Abhishek Sinhal & Rohani Raina.
+   */
+?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<input type="text" value="lata" id="searchbox" name="searchbox"/>
+<script type="text/javascript">
+<?php // ------- Defining Data Structures --------------- ?>
+DM = {};
+DM.App = {};
+DM.App.struct = {};
+DM.App.struct.song = function(){
+  this.songId = '';
+  this.songTitle = '';
+  this.songUrl = '';
+  this.songSmallPic = '';
+  this.songBigPic = '';
+  this.songMedia = '';
+};
+<?php // ------------------------------------------------ ?>
+
+<?php // ------- Defining Data Constants --------------- ?>
+DM.App.mediaSource = [];
+DM.App.mediaSource['yt'] = {
+  'id': 'youtube', 
+  'fetchUrl': 'http://gdata.youtube.com/feeds/api/videos?q=',
+  'pic_small': 'http://img.youtube.com/vi/{videoId}/2.jpg',
+  'pic_big': 'http://img.youtube.com/vi/{videoId}/0.jpg'
+};
+<?php // ------------------------------------------------ ?>
+
+<?php // ------- Defining Search Configuration HTML ids and functions --------------- ?>
+DM.App.search = {};
+<?php //Configuration ?>
+DM.App.search.searchButton = "searchbox";
+
+<?php //Function called when user click on Search button/ or type in search button ?>
+DM.App.search.searchButtonClicked = function(){
+  var query = $("#"+DM.App.search.searchButton).val();
+  DM.App.search.ytFetchSongs(query);
+};
+
+
+<?php //Function called to fetch songs from youtube ?>
+DM.App.search.ytFetchSongs = function(query){
+  var media = DM.App.mediaSource['yt'];
+  var songs = [];
+  $.getJSON(media.fetchUrl+query+'&alt=json-in-script&callback=?&max-results=12&start-index=1',
+    function(data){
+      $.each(data.feed.entry, function(i, item) {
+        console.log(item);
+        var video = item['id']['$t'];
+        video = video.replace('http://gdata.youtube.com/feeds/api/videos/','http://www.youtube.com/watch?v=');  //replacement of link
+        var videoID = video.replace('http://www.youtube.com/watch?v=',''); // removing link and getting the video ID
+
+        var song = new DM.App.struct.song();
+        song.songId = videoID;
+        song.songTitle = item['title']['$t'];
+        song.songUrl = video;
+        song.songSmallPic = media.pic_small.replace('{videoId}',videoID);
+        song.songBigPic = media.pic_big.replace('{videoId}',videoID);
+        song.songMedia = media.id;
+        songs.push(song);	
+      });
+      DM.App.homeTab.populate(songs);
+  });
+};
+<?php // ------------------------------------------------ ?>
+
+
+<?php // ------- Defining Home Tab functionality --------------- ?>
+DM.App.homeTab = {};
+DM.App.homeTab.populate = function(data){
+  for(var i = 0; i < data.length; i++) {
+	  console.log(data[i]);
+  }	
+};
+<?php // ------------------------------------------------ ?>
+
+DM.App.search.searchButtonClicked();
+</script>
